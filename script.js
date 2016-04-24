49a77ca681cfea6e29501ed46e99833053791bbb
Stephen Mayeux <stephenmayeux@gmail.com>
2016-04-24 16:02:41 +0900

    custom javascript for tindr app

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+var koreanCities = ["Busan", "Seoul", "Masan", "Daegu", "Gimhae", "Ulsan", "Jejudo"];
+
+var myLoveProfile = {
+	name: "Stephen",
+	single: false,
+	age: 29,
+	currentCity: koreanCities[0]
+};
+
+var possibleMatch= {
+	name: "Ji-yeon",
+	single: true,
+	age: 23,
+	currentCity: koreanCities[0]
+};
+
+$('#loveButton').on('click', function() {
+  if (myLoveProfile.currentCity === possibleMatch.currentCity && myLoveProfile.single && possibleMatch.single) {
+  	$('h1').('You are a match!');
+  } else if (myLoveProfile.currentCity !== possibleMatch.currentCity && myLoveProfile.single && possibleMatch.single) {
+  	$('h1').text('Do you like long distance?');
+  } else if (!myLoveProfile.single && myLoveProfile.currentCity !== possibleMatch.currentCity) {
+  	$('h1').text('You are a cheater!!');
+  } else {
+  	$('.message').append('<h1 style="color: red">You are not a match</h1>');
+  }
+});
+
+
+
+
+
+
+});
